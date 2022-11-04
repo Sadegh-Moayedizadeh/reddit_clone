f065f098287036ff46e50885bfd9ea10c2c4279d
sadegh <sadegh.moayedizadeh@gmail.com>
2022-11-04 18:55:23 +0330

    Create models and migrate.

--- src/reddit_clone/posts/models.py
+++ src/reddit_clone/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    poster = models.ForeignKey(User, models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+
+class Vote(models.Model):
+    voter = models.ForeignKey(User, models.CASCADE)
+    post = models.ForeignKey(Post, models.CASCADE)
